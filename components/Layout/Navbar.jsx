9bfd54bd12a547f9c7683780a815e660d432c349
ishola360 <isholah360.com>
2024-11-13 08:00:48 +0100

    fixtranslate

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import { IoIosSearch } from "react-icons/io";
 import Link from "next/link";
 import LanguageSwitcher from "../lang/languageSwitcher";
